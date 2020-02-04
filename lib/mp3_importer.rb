6826b3238642f41a1390ab28dbdbb70ebc455fd7
Chloe Gell <gellert.chloe@gmail.com>
2020-02-04 05:21:01 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,13 +5,13 @@ class MP3Importer
   def initialize(path)
     @path = path 
   end 
-  def files(path)
+  def files
     Dir.chdir(@path) do |path|
       Dir.glob("*.mp3")
     end 
   end 
   binding.pry 
-  def import(path)
+  def import
     files.each do |filename| 
       Song.new_by_filename(filename)
     end 
